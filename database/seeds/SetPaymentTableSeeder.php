af1f7238880c45c159f37656f3fff19e6f28d16d
ismail16 <ismail32cse@gmail.com>
2020-02-02 22:51:02 +0600

    seeder add

---
+++ database/seeds/SetPaymentTableSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SetPaymentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('paypal_sets')->insert([
+            'title' => 'Paypal',
+            'ClientId' => '5487599592',
+            'currency' => 'USD',
+            'Secret' => 'gdfgrt1234567gvjhvjjhzjhzsjh89',
+            'Description' => 'Set Paypal Account !',
+            'Status' => 0,
+            'SandboxStatus' => 1,
+        ]);
+
+        DB::table('stripe_sets')->insert([
+            'title' => 'Stripe',
+            'SecretKey' => '5487599592',
+            'PublishableKey' => 'gdfgrt1234567gvjhvjjhzjhzsjh89',
+            'Description' => 'Set Stripe Account !',
+            'Status' => 0,
+            'SandboxStatus' => 1,
+        ]);
+
+        DB::table('two_checkout_sets')->insert([
+            'title' => 'Paypal',
+            'SellerId' => '5487599592',
+            'PublishableKey' => 'jjjjjjjjjjjjjj',
+            'PrivateKey' => 'sfjjfjsgjfgjsgafjgsj',
+            'description' => 'Set Paypal Account !',
+            'status' => 0,
+            'SandboxStatus' => 1,
+        ]);
+
+        DB::table('cash_on_delivery_sets')->insert([
+           'title' => 'Paypal',
+            'ClientId' => '5487599592',
+            'currency' => 'USD',
+            'Secret' => 'gdfgrt1234567gvjhvjjhzjhzsjh89',
+            'Description' => 'Set Paypal Account !',
+            'Status' => 0,
+            'SandboxStatus' => 1,
+        ]);
+    }
+}
--- resources/views/admin/SetPayment/paypal.blade.php
+++ resources/views/admin/SetPayment/paypal.blade.php
@@ -16,27 +16,85 @@
 
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-6">
+                        <div class="col-12">
                             <div class="form-group row">
-                                <label class="col-form-label col-md-4">Store Name </label>
-                                <input type="text" name="store_name" value="" class="form-control form-control-sm col-md-7">
+                                <label class="col-form-label col-md-3">Payment Title </label>
+                                <input type="text" name="title" value="Paypal" class="form-control form-control-sm col-md-9">
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-3">Paypal ClientId</label>
+                                <input type="text" name="ClientId" value="12548697592" class="form-control form-control-sm col-md-9">
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-3">Paypal Secret</label>
+                                <input type="text" name="Secret" value="" class="form-control form-control-sm col-md-9">
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-3">Payemnt Currency</label>
+                                <input type="text" name="currency" value="USD" class="form-control form-control-sm col-md-9">
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-3">Payemnt Description</label>
+                                <input type="text" name="Description" value="Description" class="form-control form-control-sm col-md-9">
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-3">Paypal Account Type</label>
+                                <select class="form-control form-control form-control-sm col-md-9" name="SandboxStatus">
+                                  <option>Sandbox Account</option>
+                                  <option>Live Account</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-group row">
+                                <label class="col-form-label col-md-3">Paypal Activation</label>
+                                <select class="form-control form-control form-control-sm col-md-9" name="Status">
+                                  <option value="0">Paypal Deactive as Payment Method</option>
+                                  <option value="1">Paypal Active as Payment Method</option>
+                                </select>
                             </div>
                         </div>
 
 
-                        <div class="col-6">
+                        <!-- <div class="col-6">
                             <div class="form-group row">
-                                <label class="col-form-label col-md-4">Store title </label>
-                                <input type="text" name="store_title" value="" class="form-control form-control-sm col-md-7">
+                                <label class="col-form-label col-md-6"></label>
+                                <div class="icheck-primary d-inline col-md-6">
+                                    <input type="checkbox" id="checkboxPrimary3">
+                                    <label for="checkboxPrimary3">
+                                        Sandbox Active
+                                    </label>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-6">
                             <div class="form-group row">
-                                <label class="col-form-label col-md-4">Store Phone</label>
-                                <input type="text" name="store_phone" value="" class="form-control form-control-sm col-md-7">
+                                <label class="col-form-label col-md-4"></label>
+                                <div class="icheck-primary d-inline col-md-8">
+                                    <input type="checkbox" id="checkboxPrimary3">
+                                    <label for="checkboxPrimary3">
+                                        Is Paypal Active as Payment Method
+                                    </label>
+                                </div>
                             </div>
-                        </div>
+                        </div> -->
 
                     </div>
                 </div>
